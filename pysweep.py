dd31c6e6936e8b769770a9f54d48970bece1a671
Erol Ziya <erol.ziya@gmail.com>
2014-06-16 16:53:13 +0100

    Made code pass first basic set of tests

--- pysweep.py
+++ pysweep.py
@@ -6,3 +6,12 @@ class PySweeper(object):
         self.rows = []
         self.rowSize = rows
         self.colSize = cols
+
+    def board(self):
+        theboard = []
+        for rowindex in range(0, self.rowSize):
+            row = ""
+            for colindex in range(0, self.colSize):
+                row = row + "."
+            theboard.append(row)
+        return theboard
